0ffd91e431d7fe64d841051b5f37c44e401292d3
Prayash Thapa <prayash@DESKTOP-LMTQV76.>
2024-12-18 16:44:51 +0000

    update: data creation and streaming to firehose complete

--- python_consumer/main.py
+++ python_consumer/main.py
@@ -1,16 +1,29 @@
-﻿# This is a sample Python script.
+﻿import os
+import time
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import boto3
+import dotenv
 
+from src.model_communicator_service import FoundationModel
+from src.producer_service import stream_data
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+dotenv.load_dotenv()
 
+access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+region = os.getenv('AWS_REGION')
 
-# Press the green button in the gutter to run the script.
+firehose_client = boto3.client('firehose', aws_access_key_id=access_key_id,
+                               aws_secret_access_key=access_key, region_name=region)
+
+# Define the delivery stream name
+delivery_stream_name = "dataeng-firehose-streaming-s3"
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    client_bedrock = boto3.client('bedrock-runtime',
+                                  aws_access_key_id=access_key_id,
+                                  aws_secret_access_key=access_key, region_name=region)  # Use your region
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    foundation_model = FoundationModel(client_bedrock)
+
+    stream_data(firehose_client, 'dataeng-firehose-streaming-s3', foundation_model)
--- python_consumer/src/producer_service.py
+++ python_consumer/src/producer_service.py
@@ -1,6 +1,10 @@
 ﻿import os
+import time
+
 import boto3
 import dotenv
+
+from src.model_communicator_service import FoundationModel
 
 dotenv.load_dotenv()
 
@@ -8,3 +12,13 @@ access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 region = os.getenv('AWS_REGION')
 
+
+def stream_data(client, stream_, foundation_model_: FoundationModel):
+    message = foundation_model_.get_fake_letter()
+    while True:
+        response = client.put_record(
+            DeliveryStreamName=stream_,
+            Record={"Data": f'{{"message":"{message}"}}'}
+        )
+        print("PutRecord Response:", response)
+        time.sleep(10)
