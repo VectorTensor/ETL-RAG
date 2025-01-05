8d5c22d04a78b8d91a56e7047754ecebd50327b9
Prayash Thapa <comppro54@gmail.com>
2025-01-05 15:52:37 +0545

    feat(test): add tests and ci

---
+++ load_go/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	_ "gorm.io/driver/postgres"
+)
+
+type dbData  struct{
+
+  Name string
+  Email string 
+
+ 
+}
+
+
+
+func  InsertData[T any](data T,db *gorm.DB) *gorm.DB {
+
+  res := db.Create(&data)
+
+  return res
+
+}
+
+func main(){
+
+  _ = godotenv.Load()
+  host:=os.Getenv("host")
+  user:=os.Getenv("user")
+  password:=os.Getenv("password")
+  dbname:=os.Getenv("dbname")
+
+
+  fmt.Println("connecting")
+  // connStr := "postgres://prat:party%40me@localhost:5432/dummydb?sslmode=disable"
+  // dsn := "host=localhost user=prat password=party@me dbname=dummydb sslmode=disable"
+  dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user,password, dbname)
+
+  db, _ :=  gorm.Open(postgres.Open(dsn),&gorm.Config{})
+
+  fmt.Println("opened")
+  // if err != nil{
+  //
+  //   fmt.Println("Sorry some error occured !!")
+
+  fmt.Println("connected")
+  _ = db.AutoMigrate(&dbData{})
+
+  data := dbData{Name:"Prats",Email:"mnchad@Me"}
+
+  res := InsertData(data,db)
+
+  fmt.Println(res)
+
+}
+  
+
+
+
+
+
---
+++ source/tests/generator_working.py
@@ -0,0 +1,17 @@
+import unittest
+from generate_fake_transaction_data import generate_fake_sales_data
+
+class TestGenerateListFunction(unittest.TestCase):
+    def test_generate_list_size(self):
+        # Define test cases
+        test_cases = [0, 1, 5, 10, 100]
+
+        for size in test_cases:
+            with self.subTest(size=size):
+                result = generate_fake_sales_data(size)
+                # Check if the result is a list
+                self.assertIsInstance(result, list, f"Expected a list but got {type(result)}")
+                # Check if the size matches the input
+                self.assertEqual(len(result), size, f"Expected list of size {size} but got {len(result)}")
+
+
